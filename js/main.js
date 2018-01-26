7023642b9333b6b5abd1c129e9202df4539fd457
Anastasiia Somchyk <e4nastya@gmail.com>
2018-01-26 14:20:36 -0600

    added 102

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ console.log("hello main.js");
 let skyIsBlue = true;
 if (!skyIsBlue){
 
-    console.log("yeah the sky is blue");
+console.log("yeah the sky is blue");
 }else{
-    console.log("hmmm, what color is the sky?");
+console.log("hmmm, what color is the sky?");
 }
 
 let jedi = ["Skywalker", "Yoda", "Obi-wan", "Qui-Gon", "Windu"];
@@ -19,8 +19,8 @@ jedi.unshift("My Sejf");
 console.log("howmany jegdi", jedi.length);
 
 for (let potato = 0; potato < jedi.length; potato++) {
-    console.log("what x", potato);
-    console.log(jedi[potato]);
+console.log("what x", potato);
+console.log(jedi[potato]);
 
 // jediText = "<li>" what is this: " + jedi[potato] + "</li>";
 
@@ -43,10 +43,10 @@ let numbers = [2, 4, 100, 33, 54, 1, 6];
 //console.log("numbers sorted", number.sort());
 console.log("numbers:", numbers);
 let sortedNumbers = number.sort(function(first, second){
-    // console.log("first:", first, "second:", second);
-     //console.log("first mines second", first - second);
-     //console.log("change", numbers);
-     return second - first;
+// console.log("first:", first, "second:", second);
+//console.log("first mines second", first - second);
+//console.log("change", numbers);
+return second - first;
 });
 
 let joinedColors = colord.join();
@@ -63,10 +63,10 @@ for (let i = 100; i >= 0; i--){
 console.log("i modulus", i, i % 3);
 if (i % 2 === 0){
 
-    evenOdds.unshift(i);
+evenOdds.unshift(i);
 
 }else{
-    evenOdds.push(i);
+evenOdds.push(i);
 }
 
 }
